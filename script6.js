c4d6665e280454bda22b3aa62d1878b34c93de33
erimk21 <9019259@student.zadkine.nl>
2022-10-12 13:29:10 +0200

    opzet 6.1

---
+++ script6.js
@@ -0,0 +1,9 @@
+function changeColor() {
+   alert("Het Werkt")
+    console.log("het werkt")
+}
+function Calculator(num, num2) {
+ let answer = num + num2;
+ console.log(answer)
+ alert("answer");
+}
